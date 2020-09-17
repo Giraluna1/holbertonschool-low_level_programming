61ee86b9a359c9cc4db6069c313f79db263025ab
Luna Gomez <2344@holbertonschool.com>
2020-09-17 01:52:03 +0000

    C Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+/**
+ * main - main is void
+ * puts - writes the string s and a trailing newline to stdout
+ * Return: end value 0
+*/
 
-int main()
-
+int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("Programming is like building a multilingual puzzle\");
+	Return(0);
 
 }
