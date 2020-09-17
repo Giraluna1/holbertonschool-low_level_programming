f632dd8c9e149cb71682ac3670dd879df28a38e0
Luna Gomez <2344@holbertonschool.com>
2020-09-17 02:42:16 +0000

    Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main is void
+ * puts - writes the string s and a trailing newline to stdout
+ * Return: end value 0
+*/
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
