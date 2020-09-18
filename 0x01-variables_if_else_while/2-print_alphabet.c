1d00543109bd7485e736bf0564180b28d6171742
Luna Gomez <2344@holbertonschool.com>
2020-09-18 04:34:00 +0000

    Hello from

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - is void
+ * putchar - with an output stream of stdout
+ * Return: value 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	/*char ch[26] = "abcdefghijklmnopqrstuvwxyz"; */
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
