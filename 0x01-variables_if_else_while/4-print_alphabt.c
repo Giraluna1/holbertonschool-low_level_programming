101563c08f4752aaa3d2cff2e69995a2d8fe8431
Luna Gomez <2344@holbertonschool.com>
2020-09-18 05:03:13 +0000

    Hello for and if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+/**
+ * main - is void
+ * putchar - with an output stream of stdout
+ * Return: value 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	/*char ch[26] = "abcdefghijklmnopqrstuvwxyz"; */
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
