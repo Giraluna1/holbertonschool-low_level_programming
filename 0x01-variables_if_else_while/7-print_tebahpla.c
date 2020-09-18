10b05725296783ede192c3c0cec7a356f999471f
Luna Gomez <2344@holbertonschool.com>
2020-09-18 05:33:32 +0000

    --c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/**
+ * main - is void
+ * putchar - with an output stream of stdout
+ * Return: value 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
