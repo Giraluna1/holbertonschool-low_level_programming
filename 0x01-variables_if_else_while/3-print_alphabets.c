c36fe959cc876b4b554c2d229a7fdc8774748ed5
Luna Gomez <2344@holbertonschool.com>
2020-09-18 04:40:51 +0000

    Hello from with Uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+/**
+ * main - is void
+ * putchar - with an output stream of stdout
+ * Return: value 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	/*char ch[26] = "abcdefghijklmnopqrstuvwxyz"; */
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
