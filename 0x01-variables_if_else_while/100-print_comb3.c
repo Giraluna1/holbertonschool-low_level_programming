ad43504f770f1e7a4f6cdcd3843ba755d2437940
Luna Gomez <2344@holbertonschool.com>
2020-09-19 20:49:59 +0000

    i=n+1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+/**
+ * main - is void
+ * putchar - with an output stream of stdout
+ * Return: value 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n, i;
+
+	for (n = '0'; n <= '9'; n++)
+		for (i = n + 1; i <= '9'; i++)
+	{
+		putchar(n);
+		putchar(i);
+	if (n < 57 || i < 57)
+	{
+		putchar(44);
+		putchar(32);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
