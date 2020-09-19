9337b859057fc86d632b224a205c80eebaad8bbb
Luna Gomez <2344@holbertonschool.com>
2020-09-19 22:21:08 +0000

    Three digits n, i=n+1, a=i+1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,20 +11,25 @@ int main(void)
 	int n, i, a;
 
 	for (n = '0'; n <= '7'; n++)
+	{
 		for (i = n + 1; i <= '8'; i++)
-			for (a = i + 1; a <= '9'; a++)
-	{
-		putchar(n);
-		putchar(i);
-		putchar(a);
-	if (n < 55 || i < 56 || a < 57)
-	{
-		putchar(44);
-		if (n < 57 || i < 57 || a < 57)
 		{
-		putchar(32);
+			for (a = i + 1; a <= '9'; a++)
+
+			{
+				putchar(n);
+				putchar(i);
+				putchar(a);
+				if (n < 55 || i < 56 || a < 57)
+				{
+					putchar(44);
+					if (n < 57 || i < 57 || a < 57)
+					{
+						putchar(32);
+					}
+				}
+			}
 		}
-	}
 	}
 	putchar('\n');
 	return (0);
