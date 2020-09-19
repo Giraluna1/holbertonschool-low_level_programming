780e8b8a5dfafa33b9df37f080c70ad97c95f1e1
Luna Gomez <2344@holbertonschool.com>
2020-09-19 04:24:41 +0000

    comas and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,12 @@ int main(void)
 	int n;
 
 	for (n = '0'; n <= '9'; n++)
-	{		
-	        putchar(n);     	
+	{
+		putchar(n);
 		if (n < 57)
-	       
 		{
 		putchar(44); /*44 es el valor para la "," */
-		putchar(32); /*32 es el valor del espacio */	 
+		putchar(32); /*32 es el valor del espacio */
 		}
 	}
 	putchar('\n');
