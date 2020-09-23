cf250e27b83fd4fb15cdd58bbcd02c67b5465346
Luna Gomez <2344@holbertonschool.com>
2020-09-23 01:23:40 +0000

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
-#include <ctype.h>
+
 /**
- * _isalpha - hecks whether a character is alphabet (a-z) or not.
+ * _isalpha - hecks whether a character is (a-z) or not.
  * @c: variable integre
  * Return: 0
  */
 
 int _isalpha(int c)
 {
-	if (isalpha(c))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
