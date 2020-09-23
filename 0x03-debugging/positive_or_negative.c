1f37cf6c195a4ff75c3a0eb92971067c3d17d55f
Luna Gomez <2344@holbertonschool.com>
2020-09-23 05:26:15 +0000

    debuggins

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * 
+ * Return: value is 0
+ */
+
+void positive_or_negative(int n)
+ {
+	 if (n > 0)
+	 {
+		 printf("%d is positive\n", n);
+	 }
+	 else if (n < 0)
+	 {
+		 printf("%d is negative\n", n);
+	 }
+	 if (n == 0)
+	 {
+		 printf("%d is zero\n", n);
+	 }
+
+ }
