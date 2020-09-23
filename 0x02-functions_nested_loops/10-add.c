14df53940811c6c5f283a57b3e59972c0afa58d4
Luna Gomez <2344@holbertonschool.com>
2020-09-23 04:51:07 +0000

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - adds two integers
+ * @a: number integre
+ * @b: number integre
+ * Return: c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+
+}
