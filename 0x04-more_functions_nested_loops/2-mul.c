21eefec5af41a86e6f47b076d172a4e2de19eac4
Luna Gomez <2344@holbertonschool.com>
2020-09-24 18:11:38 +0000

    multiplie two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - multiplies numbers
+ * @a: firt multipled
+ * @b: second multiped
+ * Return: c rensponse
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
