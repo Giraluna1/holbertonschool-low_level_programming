f4ace77fd30c3534ea3abf35313aa67dab9b04cd
Luna Gomez <2344@holbertonschool.com>
2020-09-24 03:14:19 +0000

    multiplication

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * times_table - multiplication
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int calculo = i * j;
+
+			if (calculo >= 10)
+			{
+				_putchar(' ');
+				_putchar ((calculo / 10) + '0');
+				_putchar ((calculo % 10) + '0');
+			}
+			else
+			{
+				_putchar (' ');
+				_putchar (' ');
+				_putchar (calculo + '0');
+			}
+			if (j != 9)
+				_putchar (',');
+		}
+
+		_putchar ('\n');
+	}
+}
