3c9be792bdd686de4c421bfeff13db0b586138d9
Luna Gomez <2344@holbertonschool.com>
2020-09-29 06:00:38 +0000

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+
+void print_to_98(int n)
+
+{
+	if (n > 98)
+	{
+		for (n = n; n >= 98; n--)
+		{
+      		printf("%d", n);
+		if(n != 98)
+			printf(", ");		
+		}
+	}
+	else 
+	{
+		for (n = n; n <= 98; n++)
+		{
+		 	printf("%d", n);
+			if(n != 98)
+				printf(", ");
+		}
+	}
+	putchar('\n');
+}
