944ef9c0b1c77688ad626ec31f29c99c292b3e92
Luna Gomez <2344@holbertonschool.com>
2020-09-30 04:40:53 +0000

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int length, half, n;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+	}
+	if (length % 2 == 0)
+	{
+		half = length / 2;
+	}
+	else
+	{
+		half = (length - 1) / 2;
+	}
+	for (n = half; n < length; n++)
+	{
+		_putchar (str[n]);
+	}
+	_putchar('\n');
+}
