4842c8911b39130af459686d25683f6da24225cc
Luna Gomez <2344@holbertonschool.com>
2020-09-30 04:16:17 +0000

    puts pares

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,10 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-		if (str[i] > 0)
+	   
+		if (i % 2 == 0)
 		{
-			i += 1;
+			_putchar(str[i]);
 		}
 	}
 	_putchar('\n');
