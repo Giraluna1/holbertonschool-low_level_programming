f196cb205fa4b4c26718bc67a473f36b80dd15a3
Luna Gomez <2344@holbertonschool.com>
2020-10-03 04:20:33 +0000

    pointers arrays strings

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,6 +1,8 @@
 #ifndef holberton
 #define holberton
 
+char *leet(char *);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
