db7b0d789ea283b76e7f4ba3ab8ec58c2af3ffb7
Luna Gomez <2344@holbertonschool.com>
2020-10-11 09:57:16 +0000

    Arguments to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - two parameters
+ * @argc: unused
+ * @argv: string
+ * Return: zero
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+/******* project AC argc argc ******/
+int main(int argc __attribute__((unused)), char *argv[]);
+
+
+#endif /*HOLBERTON_H */
