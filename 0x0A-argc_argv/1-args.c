20bb96dadad395ffac8b0eb57b16b2e3912f146e
Luna Gomez <2344@holbertonschool.com>
2020-10-11 10:22:18 +0000

    number arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - prints the number of arguments
+ * @argc: argument count
+ * @argv: unused
+ * Return: Zero
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
