bb06e24740aad83d3c5df694f0817bdf9e3e9abf
Luna Gomez <2344@holbertonschool.com>
2020-10-12 04:39:13 +0000

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * main - the minimun number of coins
+ * @argc: count
+ * @argv: value
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int counter = 0;
+	int n = 0, i;
+	int a[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n <= 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (n - a[i] >= 0)
+			{
+				counter++;
+			}
+		}
+	}
+	printf("%d\n", counter);
+	return (0);
+}
