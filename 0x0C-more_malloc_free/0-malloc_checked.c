ae8673335cad455c63cd11592f3edcd38562c6e0
Luna Gomez <2344@holbertonschool.com>
2020-10-15 18:43:02 +0000

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * malloc_checked - allocates memory
+ * @b: pointer
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
