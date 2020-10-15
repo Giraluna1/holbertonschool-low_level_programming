75f7a10e967f64656c9736bc3766eadd52a2ffec
Luna Gomez <2344@holbertonschool.com>
2020-10-15 15:59:08 +0000

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,35 +9,3 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **matrix, i, j;
-
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	matrix = malloc(height * sizeof(int *));
-
-	if (matrix == NULL)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
-	{
-		*(matrix + i) = malloc(width * sizeof(int));
-
-		if (*(matrix + i) == NULL)
-		{
-			for (j = 0; j < i; j++)
-				free(matrix[j]);
-			free(matrix);
-			return (NULL);
-		}
-	}
-
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			*(*(matrix + i) + j) = 0;
-		}
-	}
-	return (matrix);
-}
