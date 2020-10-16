0963fba05fbe7a20b9a51ca2de3643a10cbb0792
Luna Gomez <2344@holbertonschool.com>
2020-10-16 02:42:31 +0000

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _calloc - asigna memoria a un array
+ * @nmemb: amount memory
+ * @size: data type
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	for (a = 0; a < nmemb; a++)
+	{
+		ptr[a] = 0;
+	}
+	return (ptr);
+}
