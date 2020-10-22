1490758bb9b05940b4b1c83dd3dde60b9515af10
Luna Gomez <2344@holbertonschool.com>
2020-10-22 03:30:53 +0000

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,17 +17,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		for (i = 0; i < size; i++)
 		{
 			cmp(array[i]);
-			if (cmp(array[i]) == 0 || cmp(array[i]) != 0)
-			{
-				if (cmp(array[i]) == 0)
+			if (cmp(array[i]) == 0)
 				i++;
-				if (cmp(array[i]) != 0)
+			if (cmp(array[i]) != 0)
 				return (i);
-			}
-			else
-				return (-1);
 		}
 	}
-	return (i);
+	return (-1);
 }
 
