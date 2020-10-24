4cf60b4bf955f54ef28f67bd8d6c1d22f37a4062
Luna Gomez <2344@holbertonschool.com>
2020-10-24 21:58:02 +0000

    print variadic number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ for (i = 0; i < n; i++)
 		if (i < n - 1)
 			printf("%d%s", va_arg(ap, int), separator);
 		else
-			printf("%d\n", va_arg(ap, int));
+			printf("%d", va_arg(ap, int));
 	}
 	else
 		printf("%d", va_arg(ap, int));
