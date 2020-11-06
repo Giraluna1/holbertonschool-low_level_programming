8bce89eec0a60c51d93ecda5097a5173a9700741
Giraluna1 <lunagolo@hotmail.com>
2020-11-06 15:31:56 +0000

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,9 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-/*void init_dog(struct dog *d, char *name, float age, char *owner);*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
 /*void print_dog(struct dog *d);*/
-/*struct dog *new_dog(char *name, float age, char *owner);*/
+dog_t *new_dog(char *name, float age, char *owner);
 /*void free_dog(dog_t *d);*/
 
 #endif
