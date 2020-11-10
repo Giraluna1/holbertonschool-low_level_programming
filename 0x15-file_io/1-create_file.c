a9b3f6d3afdfdf03ec6136c209fc35b3833195de
Giraluna1 <lunagolo@hotmail.com>
2020-11-10 02:16:51 +0000

    Fixed closed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,10 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	if (!text_content)
 		n = write(fd, "", 1);
 	else
