8bcc3c65688fbce25f8d76c44e72bae9d4141317
Giraluna1 <lunagolo@hotmail.com>
2020-11-10 02:14:38 +0000

    Fixed write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,9 +30,11 @@ int create_file(const char *filename, char *text_content)
 
 		n = write(fd, text_content, i);
 		if (n == -1)
+		{
+			close(fd);
 			return (-1);
+		}
 	}
-
 
 	close(fd);
 	return (1);
