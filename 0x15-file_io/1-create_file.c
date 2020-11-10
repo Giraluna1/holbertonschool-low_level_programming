68fb9b31f83af2b99751f46687768ffb35bb51c9
Giraluna1 <lunagolo@hotmail.com>
2020-11-10 01:59:52 +0000

    Fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,11 +25,10 @@ int create_file(const char *filename, char *text_content)
 		n = write(fd, "", 1);
 	else
 	{
-		for ( i = 0; text_content[i] != '\0'; i++)
+		for (i = 0; text_content[i] != '\0'; i++)
 			;
 
 		n = write(fd, text_content, i);
-	
 		if (!n)
 			return (-1);
 	}
