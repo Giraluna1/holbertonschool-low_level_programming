4a2af11c0879573c891f8d848b4bc505fe9744cb
Giraluna1 <lunagolo@hotmail.com>
2020-11-10 01:57:27 +0000

    Add empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,25 +11,25 @@ int create_file(const char *filename, char *text_content)
 {
 
 	int fd;
-	int n = 0;
+	int i, n = 0;
 
 	if (!filename)
 		return (-1);
 
 
-	if (!text_content)
-	{
-		fd = open(filename, O_CREAT);
 
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (0);
-	}
+	if (!text_content)
+		n = write(fd, "", 1);
 	else
 	{
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-		if (fd == -1)
-			return (0);
+		for ( i = 0; text_content[i] != '\0'; i++)
+			;
 
-		n = write(fd, text_content, 400);
+		n = write(fd, text_content, i);
+	
 		if (!n)
 			return (-1);
 	}
