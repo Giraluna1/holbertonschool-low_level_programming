c8df5ac77f7d01354f75d245af42b8fd5129eace
Giraluna1 <lunagolo@hotmail.com>
2020-11-11 03:07:09 +0000

    Add controls

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+
+/**
+ * main - Program that copies file to another
+ * @ac: is the counter of arguments
+ * @av: is string of argument
+ * Return: Always 0;
+ */
+
+int main(int ac, char **av)
+{
+	int fd, fd2, r = 0, w = 0, c = 0, c2 = 0;
+	char *buf[1024];
+
+	if (ac != 3)
+	{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", av[0]);
+	exit(97);
+	}
+}
