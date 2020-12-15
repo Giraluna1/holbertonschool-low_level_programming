e31ff6201ac92f07b0961706240c112537414eb0
Giraluna1 <lunagolo@hotmail.com>
2020-12-14 23:50:02 -0500

    add control

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,13 +32,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		if (temp->next)
+		if (!temp->next)
+		{
+			free(*head);
+			*head = NULL;
+		}
+		else 
 		{
 			/*make prev the next as NULL and next of head as head*/
 			temp->next->prev = NULL;
 			*head = (*head)->next;
 			free(temp);
 		}
+			
 		return (1);
 	}
 	while (i < index)
