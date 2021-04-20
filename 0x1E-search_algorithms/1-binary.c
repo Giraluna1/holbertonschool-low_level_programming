91503fbbf76eb15dd639b2967566b217cfddd1be
Giraluna1 <lunagolo@hotmail.com>
2021-04-20 16:38:19 -0500

    Fix the control if value < array[mid]

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -33,7 +33,7 @@ int binary_search(int *array, size_t size, int value)
 		}
 		else if (value < array[value_mid])
 		{
-			value_right = value_mid + 1;
+			value_right = value_mid - 1;
 		}
 		else
 		{
